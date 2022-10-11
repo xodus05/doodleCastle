3d6c703b5652aac5a23bfb1f29271fb2a5c33b70
chaeyoung Moon <80873608+moon12-3@users.noreply.github.com>
2022-10-12 02:21:57 +0900

    애니메이션 추가

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -11,6 +11,12 @@ public class PlayerMove : MonoBehaviour
 
     public float runSpeed;
     private float applyRunSpeed;
+    private bool applyRunFlag = false;
+
+    public int walkCount;
+    private int currentWalkCount;
+
+    private bool canMove = true;
 
     // Start is called before the first frame update
     void Start()
@@ -18,24 +24,41 @@ public class PlayerMove : MonoBehaviour
         
     }
 
+    IEnumerator MoveCoroutine() {
+        if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
+            applyRunSpeed = runSpeed;
+            applyRunFlag = true;
+        }
+        else {
+            applyRunSpeed = 0;
+            applyRunFlag = false;
+        }
+        
+         vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+        while(currentWalkCount < walkCount) {
+            if(vector.x  != 0) {
+                transform.Translate(vector.x * (speed+applyRunSpeed), 0, 0);
+            }
+            else if(vector.y != 0) {
+                transform.Translate(0, vector.y * (speed+applyRunSpeed), 0);
+            }
+            if(applyRunFlag) currentWalkCount++; 
+            currentWalkCount++;
+             yield return new WaitForSeconds(0.01f);
+        }
+        currentWalkCount = 0;
+       
+        canMove = true;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
-            applyRunSpeed = runSpeed;
-        }
-        else {
-            applyRunSpeed = speed;
-        }
-
-        if(Input.GetAxisRaw("Horizontal")!=0 || Input.GetAxisRaw("Vertical") !=0) {
-            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
-
-            if(vector.x  != 0) {
-                transform.Translate(vector.x * applyRunSpeed, 0, 0);
-            }
-            else if(vector.y != 0) {
-                transform.Translate(0, vector.y * applyRunSpeed, 0);
+        if(canMove) {
+            if(Input.GetAxisRaw("Horizontal")!=0 || Input.GetAxisRaw("Vertical") !=0) {
+                canMove = false;
+                StartCoroutine(MoveCoroutine());
             }
         }
     }
