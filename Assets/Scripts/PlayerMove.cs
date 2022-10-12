ac4e958fb11ce7f3880d232929818a5d8e9fca8d
chaeyoung Moon <80873608+moon12-3@users.noreply.github.com>
2022-10-12 23:15:16 +0900

    캐릭터 애니메이션 적용 완료

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -9,46 +9,58 @@ public class PlayerMove : MonoBehaviour
 
     private Vector3 vector;
 
-    public float runSpeed;
+    public float runSpeed; 
     private float applyRunSpeed;
-    private bool applyRunFlag = false;
+    private bool applyRunFlag = false;  // 달릴 때 2칸을 가지 않기 위해 조절
 
     public int walkCount;
     private int currentWalkCount;
 
     private bool canMove = true;
 
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     IEnumerator MoveCoroutine() {
-        if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
-            applyRunSpeed = runSpeed;
-            applyRunFlag = true;
-        }
-        else {
-            applyRunSpeed = 0;
-            applyRunFlag = false;
-        }
-        
-         vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+        while(Input.GetAxisRaw("Vertical")!=0 || Input.GetAxisRaw("Horizontal") != 0)   // 연속 걷기 시 애니메이션이 계속 실행되도록
+        {
+            if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {   // SHIFT를 눌렀을 시 달리기
+                applyRunSpeed = runSpeed;
+                applyRunFlag = true;
+            }
+            else {
+                applyRunSpeed = 0;
+                applyRunFlag = false;
+            }
+            
+            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z); // 설정
 
-        while(currentWalkCount < walkCount) {
-            if(vector.x  != 0) {
-                transform.Translate(vector.x * (speed+applyRunSpeed), 0, 0);
+            if(vector.x != 0) vector.y = 0;
+
+            // 애니메이션 설정
+            animator.SetFloat("DirX", vector.x);
+            animator.SetFloat("DirY", vector.y);
+            animator.SetBool("Walking", true);
+
+            while(currentWalkCount < walkCount) {   // 한 칸을 가기 위한 반복문
+                if(vector.x  != 0) {
+                    transform.Translate(vector.x * (speed+applyRunSpeed), 0, 0);   // x가 0이 아니면 위치 이동
+                }
+                else if(vector.y != 0) {
+                    transform.Translate(0, vector.y * (speed+applyRunSpeed), 0);
+                }
+                if(applyRunFlag) currentWalkCount++; 
+                currentWalkCount++; // 한 칸을 가기 위한 반복문
+                yield return new WaitForSeconds(0.01f); // 천천히 모션을 실행하기 위한 딜레이값
             }
-            else if(vector.y != 0) {
-                transform.Translate(0, vector.y * (speed+applyRunSpeed), 0);
-            }
-            if(applyRunFlag) currentWalkCount++; 
-            currentWalkCount++;
-             yield return new WaitForSeconds(0.01f);
+            currentWalkCount = 0;
         }
-        currentWalkCount = 0;
-       
+        animator.SetBool("Walking", false);
         canMove = true;
     }
 
